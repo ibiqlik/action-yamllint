7cb973d6a30c83d7196d8b1e8f06a13393b1fd3b
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2019-10-16 10:47:45 +0200

    Add flags when on of the Configs is specified

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh -l
 
-if [ ! -z $INPUT_STRICT ]; then
+if [ ! -z "$INPUT_STRICT" ]; then
     STRICT="-s"
 fi
 
-yamllint $INPUT_CONFIG_FILE $INPUT_CONFIG_DATA $INPUT_FORMAT $STRICT $INPUT_FILE_OR_DIR 
+if [ ! -z "$INPUT_CONFIG_FILE" ]; then
+    CONFIG_FILE="-c $INPUT_CONFIG_FILE"
+fi
+
+if [ ! -z "$INPUT_CONFIG_DATA" ]; then
+    CONFIG_DATA="-d $INPUT_CONFIG_DATA"
+fi
+
+yamllint $CONFIG_FILE $CONFIG_DATA $INPUT_FORMAT $STRICT $INPUT_FILE_OR_DIR 
